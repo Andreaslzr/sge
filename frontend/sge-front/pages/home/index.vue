371bc22f16c83c3ccb4731ac02d91bb5251c8e69
Desenvolvimento <dsadm@educ501.local>
2024-03-19 20:14:09 -0300

    sge

---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+definePageMeta({
+    middleware: 'auth'
+})
+
+const { signOut } = useAuth();
+
+const submitLogout = ()=>{
+    signOut({redirect: false}).then(()=>{
+                console.log("Sucessfully logout!")
+                navigateTo('/');
+            })
+            .catch((error)=>{
+                console.log("Error when trying to logout: ", error);
+            });
+}
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!</h1>
+        <button @click="submitLogout" class="customButton">SAIR</button>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
